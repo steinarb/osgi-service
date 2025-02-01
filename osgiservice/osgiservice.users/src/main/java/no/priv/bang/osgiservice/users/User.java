d92888632de2de0e33ad3ad16f13ed696e59af29
Steinar Bang <sb@dod.no>
2025-02-01 12:31:08 +0100

    Add fields numberOfFailedLogins and isLocked to User record

--- osgiservice/osgiservice.users/src/main/java/no/priv/bang/osgiservice/users/User.java
+++ osgiservice/osgiservice.users/src/main/java/no/priv/bang/osgiservice/users/User.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2024 Steinar Bang
+ * Copyright 2019-2025 Steinar Bang
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,7 +18,15 @@ package no.priv.bang.osgiservice.users;
 /**
  * Bean used to in {@link UserManagementService} operations to represent a user in the database.
  */
-public record User (int userid, String username, String email, String firstname,String lastname) {
+public record User (
+    int userid,
+    String username,
+    String email,
+    String firstname,
+    String lastname,
+    int numberOfFailedLogins,
+    boolean isLocked)
+{
 
     public static Builder with() {
         return new Builder();
@@ -31,6 +39,8 @@ public record User (int userid, String username, String email, String firstname,
         builder.email = user.email;
         builder.firstname = user.firstname;
         builder.lastname = user.lastname;
+        builder.numberOfFailedLogins = user.numberOfFailedLogins;
+        builder.isLocked = user.isLocked;
         return builder;
     }
 
@@ -40,11 +50,13 @@ public record User (int userid, String username, String email, String firstname,
         private String email;
         private String firstname;
         private String lastname;
+        private int numberOfFailedLogins;
+        private boolean isLocked;
 
         private Builder() {}
 
         public User build() {
-            return new User(this.userid, this.username, this.email, this.firstname, this.lastname);
+            return new User(userid, username, email, firstname, lastname, numberOfFailedLogins, isLocked);
         }
 
         public Builder userid(int userid) {
@@ -69,6 +81,16 @@ public record User (int userid, String username, String email, String firstname,
 
         public Builder lastname(String lastname) {
             this.lastname = lastname;
+            return this;
+        }
+
+        public Builder numberOfFailedLogins(int failedLogins) {
+            this.numberOfFailedLogins = failedLogins;
+            return this;
+        }
+
+        public Builder isLocked(boolean isLocked) {
+            this.isLocked = isLocked;
             return this;
         }
     }
--- osgiservice/osgiservice.users/src/test/java/no/priv/bang/osgiservice/users/UserTest.java
+++ osgiservice/osgiservice.users/src/test/java/no/priv/bang/osgiservice/users/UserTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2024 Steinar Bang
+ * Copyright 2019-2025 Steinar Bang
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,18 +28,24 @@ class UserTest {
         var email = "jdoe31@gmail.com";
         var firstname = "John";
         var lastname = "Doe";
+        var numberOfFailedLogins = 4;
+        var isLocked = true;
         var user = User.with()
             .userid(userid)
             .username(username)
             .email(email)
             .firstname(firstname)
             .lastname(lastname)
+            .numberOfFailedLogins(numberOfFailedLogins)
+            .isLocked(isLocked)
             .build();
         assertEquals(userid, user.userid());
         assertEquals(username, user.username());
         assertEquals(email, user.email());
         assertEquals(firstname, user.firstname());
         assertEquals(lastname, user.lastname());
+        assertEquals(numberOfFailedLogins, user.numberOfFailedLogins());
+        assertEquals(isLocked, user.isLocked());
     }
 
     @Test
@@ -49,12 +55,16 @@ class UserTest {
         var email = "jdoe31@gmail.com";
         var firstname = "John";
         var lastname = "Doe";
+        var numberOfFailedLogins = 4;
+        var isLocked = true;
         var user = User.with()
             .userid(userid)
             .username(username)
             .email(email)
             .firstname(firstname)
             .lastname(lastname)
+            .numberOfFailedLogins(numberOfFailedLogins)
+            .isLocked(isLocked)
             .build();
         var copy = User.with(user).build();
         assertEquals(userid, copy.userid());
@@ -62,6 +72,8 @@ class UserTest {
         assertEquals(email, copy.email());
         assertEquals(firstname, copy.firstname());
         assertEquals(lastname, copy.lastname());
+        assertEquals(numberOfFailedLogins, copy.numberOfFailedLogins());
+        assertEquals(isLocked, copy.isLocked());
     }
 
     @Test
@@ -72,6 +84,8 @@ class UserTest {
         assertNull(user.email());
         assertNull(user.firstname());
         assertNull(user.lastname());
+        assertEquals(0, user.numberOfFailedLogins());
+        assertEquals(false, user.isLocked());
     }
 
 }
