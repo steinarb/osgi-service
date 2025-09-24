96f3cc4e8505eb97c55bdfc9a56ae1d6450f14ea
Steinar Bang <sb@dod.no>
2025-09-24 22:50:38 +0200

    Remove public declaration from junit jupiter test class

--- osgiservice/osgiservice.users/src/test/java/no/priv/bang/osgiservice/users/UserManagementConfigTest.java
+++ osgiservice/osgiservice.users/src/test/java/no/priv/bang/osgiservice/users/UserManagementConfigTest.java
@@ -19,7 +19,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class UserManagementConfigTest {
+class UserManagementConfigTest {
     @Test
     void testCreate() {
         int excessiveFailedLoginLimit = 3;
